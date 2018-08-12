af7cb9d67e204d0f6e3d895f14f0ca3a5fb6be48
Thierry <thierry.rais@gmail.com>
2018-08-12 11:42:53 +0200

    Create camelCase.js

---
+++ camelCase.js
@@ -0,0 +1,8 @@
+// Camel-to-Title
+// Convert a given sentence from camelCase to Title Case
+const camelCase = "the simplestThings in LIFE are alwaysThe best"
+const titleCase = text => {
+//   Insert function
+
+}
+titleCase(camelCase)
