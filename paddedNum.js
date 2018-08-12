3cf4b277a9f333e802790625a68f2e84aef3ad24
Thierry <thierry.rais@gmail.com>
2018-08-12 11:41:59 +0200

    Create paddedNum.js

---
+++ paddedNum.js
@@ -0,0 +1,10 @@
+// Write a function to manipulate the provided string
+
+// 1. Padded Number
+// Pad given single numbers in array to look like "10", "05", "16", "02"
+const nums = ['2', '4', '25', '10', '3']
+const paddedNum = text => {
+//   Insert function
+
+}
+paddedNum(nums)
