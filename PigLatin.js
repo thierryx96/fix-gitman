3aedbb02f669c6782c78b9a31a633cbbc69b5d3e
Thierry <thierry.rais@gmail.com>
2018-08-12 12:06:20 +0200

    Update and rename PigLatin.js to intersect.js

--- PigLatin.js
+++
@@ -1,8 +0,0 @@
-// Pig Latin
-// Convert any word to Pig Latin, see how to convert here => https://en.wikipedia.org/wiki/Pig_Latin
-const word = "hamlet"
-const pigLatin = text => {
-//   Insert function
-  
-}
-pigLatin(word);
---
+++ intersect.js
@@ -0,0 +1,23 @@
+/*
+Write a program that returns a list that contains only the elements that are common between the lists (without duplicates). 
+
+Take two lists, say for example these two:
+
+[1, 1, 2, 3, 5, 8]
+[3, 4, 5, 6, 7, 8, 9] 
+
+Should returns :
+[3, 5]
+*/
+
+// 2) Title-to-Camel
+// Convert the Title Case back to camelCase
+const l1 = [1, 1, 2, 3, 5, 8];
+const l2 = [3, 4, 5, 6, 7, 8, 9];
+
+const intersect = (a, b) => {
+  //   Insert function
+
+}
+
+console.log(intersect(l1, l2)) //[3, 5]
