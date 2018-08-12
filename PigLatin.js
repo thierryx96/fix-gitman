5ef976132273bde6fa0de0fd7c9779f64a9292a4
Thierry <thierry.rais@gmail.com>
2018-08-12 11:43:29 +0200

    Create PigLatin.js

---
+++ PigLatin.js
@@ -0,0 +1,8 @@
+// Pig Latin
+// Convert any word to Pig Latin, see how to convert here => https://en.wikipedia.org/wiki/Pig_Latin
+const word = "hamlet"
+const pigLatin = text => {
+//   Insert function
+  
+}
+pigLatin(word);
